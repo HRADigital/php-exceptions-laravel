5724185d9bfb3befec323d057d15303ee86fb6e2
HRADigital <github@hradigital.com>
2026-08-20 13:51:54 +0100

    chore: relicense package under MPL-2.0 and add a trademark notice

--- src/Renderers/DefaultRenderer.php
+++ src/Renderers/DefaultRenderer.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) HRADigital - Hugo Rafael Azevedo.
+ */
+
 declare(strict_types=1);
 
 namespace HraDigital\Components\ExceptionRenderer\Renderers;
